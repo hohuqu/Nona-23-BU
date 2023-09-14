a82471a253787739b996535b58f9e0bf89c9f7ad
Taos Transue <spacechips@rocketmail.com>
2023-09-13 18:55:02 -0600

    restriction progress

--- Twist Problem/bio.py
+++ Twist Problem/bio.py
@@ -1,9 +1,26 @@
 from collections import defaultdict
 import re
 import itertools
+from typing import NamedTuple
 import pandas as pd
 from Bio.Seq import Seq
 import Bio.Restriction as rst
+
+
+class Fragment(NamedTuple):
+    seq: Seq
+    end5: rst.RestrictionType
+    end3: rst.RestrictionType
+
+    def can_ligate(self, other):
+        compat = self.end3.compat % other.end5.compat
+        overhang_zero = self.overhang + other.overhang == 0
+        return compat and overhang_zero
+
+    def ligate(self, other):
+        if not self.can_ligate(other):
+            raise ValueError('Cannot ligate')
+        return Fragment(self.seq + other.seq, self.end5, other.end3)
 
 
 def is2s(e):
@@ -18,38 +35,47 @@ def is2s(e):
     return False
 
 
-def enzyme_equivalences(enzymes):
+def equivalences(stuff, equiv_rel):
     equivalences = []
-    for e in enzymes:
+    for e in stuff:
         found = False
         for equiv in equivalences:
-            if e % equiv[0]:
+            if equiv_rel(e, equiv[0]):
                 equiv.append(e)
                 found = True
         if not found:
             equivalences.append([e])
-    return {str(e[0]): e for i, e in enumerate(equivalences)}
+    return {e[0]: e for i, e in enumerate(equivalences)}
 
 
-def enzyme_equiv_matrix():
-    # equivs = [e1 % e2 for e1, e2 in itertools.product(enzymes, enzymes)]
-    enzyme_names = rst.AllEnzymes.elements()
-    df = pd.DataFrame(columns=enzyme_names, index=enzyme_names, dtype=bool)
-    df[:] = False
-    for e1, e2 in itertools.product(enzyme_names, enzyme_names):
-        if rst.AllEnzymes.get(e1) % rst.AllEnzymes.get(e2):
-            df[e1].loc[e2] = True
-    return df
+def chew_seq(seq, enzymes, frags):
+    prev_e = None
+    blunt_example = rst.BsrBI
+    for i, e in enumerate(enzymes):
+        s1, s2 = e.catalyze(seq)
+        seq = s2
+        if i == 0:
+            e1 = rst.BsrBI
+            e2 = e
+        elif i == len(enzymes) - 1:
+            e1 = prev_e
+            e2 = rst.BsrBI)
+        else:
+            e1 = prev_e
+            e2 = End(..., e)
+        prev_e = e
+        frags.append(Fragment(Seq(s1), e1, e2))
+    return frags
 
 
 def digestion_ligation(seqs, enzymes_2p, equiv_2p):
     analyses = {i: rst.Analysis(enzymes_2p, s) for i, s in enumerate(seqs)}
-    equiv_classes = defaultdict(list)
+    equiv_classes = defaultdict(set)
     for i, a in analyses.items():
-        for e in a.with_sites().keys():
-            for k in (rst.AllEnzymes.get(k) for k in equiv_2p):
-                if e % k:
-                    equiv_classes[i].append(str(e))
+        for e in a.with_sites():
+            k = next((k for k in equiv_2p if e % k), None)
+            if k:
+                equiv_classes[i].add(k)
     breakpoint()
     assert all(len(s) == 1 for s in equiv_classes.values()), equiv_classes
 
@@ -59,8 +85,9 @@ def main():
     # mat = enzyme_equiv_matrix()
     enzymes_2p = [e for e in rst.AllEnzymes if not is2s(e)]
     enzymes_2s = [e for e in rst.AllEnzymes if is2s(e)]
-    equiv_2p = enzyme_equivalences(enzymes_2p)
-    equiv_2s = enzyme_equivalences(enzymes_2s)
+    isoclass = equivalences(enzymes_2p, lambda e1, e2: (e1 % e2) and e1.is_equischizomer(e2))
+    compatclass = equivalences(isoclass.keys(), lambda e1, e2: e1 % e2)
+    breakpoint()
     seq1 = Seq('AAAAAGATCCAAAAAA')
     seq2 = Seq('TTTTGGATCCTTTTTT')
     digestion_ligation([seq1, seq2], enzymes_2p, equiv_2p)
